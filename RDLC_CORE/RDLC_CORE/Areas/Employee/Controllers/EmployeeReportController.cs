2e28f15f3fcf944c97b7cc79f6c299cf74288ded
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2022-12-25 19:41:44 +0600

    App

---
+++ RDLC_CORE/RDLC_CORE/Areas/Employee/Controllers/EmployeeReportController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using System.Composition;
+using System.Data;
+
+namespace RDLC_CORE.Areas.Employee.Controllers
+{
+    public class EmployeeReportController : Controller
+    {
+    
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public EmployeeReportController(IWebHostEnvironment webHostEnvironment)
+        {
+
+            _webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            var dt = new DataTable();
+            dt = report.GetReportList();  //Get Store procedure result set
+
+            string mimeType = "";
+            int extension = 1;
+            var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\ReportExpense.rdlc";
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm1", "RDCL Report");
+            parameters.Add("prm2", DateTime.Now.ToString("dd-MM-yyyy"));
+            parameters.Add("prm3", "Expense Report");
+
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("DataSetName", dt);  //This DataSetName must be used  in .rdlc report datasetName
+
+            var res = localReport.Execute(RenderType.Pdf, extension, parameters, mimeType);
+            return File(res.MainStream, "application/pdf");
+            //return File(result.MainStream, "application/msexcel", "Export.xls");
+        }
+
+    }
+}
--- RDLC_CORE/RDLC_CORE/Program.cs
+++ RDLC_CORE/RDLC_CORE/Program.cs
@@ -1,7 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Reflection.PortableExecutable;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
+builder.WebHost.UseWebRoot("wwwroot");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddHttpContextAccessor();
+
+#region Areas Config
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Areas/{2}/{0}" + RazorViewEngine.ViewExtension);
+    options.AreaViewLocationFormats.Add("/areas/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/areas/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+    // {0} - Action Name
+    // {1} - Controller Name
+    // {2} - Area Name
+});
+
+
+#endregion
+
+
 
 var app = builder.Build();
 
@@ -19,9 +46,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "MyArea",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
